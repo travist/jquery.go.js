92ae221bb4885ae913c02327372f24979eef9e4a
Travis Tidwell <travist349@gmail.com>
2013-08-18 13:23:36 -0500

    Modifying the code so that it can support the each method.

---
+++ examples/example3.js
@@ -0,0 +1,19 @@
+var async = require('async');
+var $ = require('../lib/jquery.go.js');
+
+// Add some default configs.
+$.config.site = 'http://localhost:8888';
+$.config.addJQuery = false;
+
+// Using each method.
+$.visit('/node', function() {
+  $('h2 a').each(function(index, element, done) {
+    this.text(function(text) {
+      console.log(text);
+      done();
+    });
+  }, function() {
+    console.log('done');
+    $.close();
+  });
+});
